4374a9c15930e6a0fab2b72d71cf94af287bfbdf
alxmart <alxmartinez@gmail.com>
2024-02-23 09:50:35 -0300

    Defined Intent and startActivity

--- app/src/main/java/com/luizafmartinez/m14_calculadoraimc/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/m14_calculadoraimc/MainActivity.kt
@@ -1,11 +1,54 @@
 package com.luizafmartinez.m14_calculadoraimc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btnCalcular: Button
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnCalcular = findViewById(R.id.btn_calcular)
+
+        btnCalcular.setOnClickListener {
+
+            val intent = Intent(
+                this,
+                ResultadoActivity::class.java
+            )
+
+            // Passar parâmetros para outra activity
+            /*
+             intent.putExtra("filme","The Witcher")
+             intent.putExtra("classificacao",5)
+             intent.putExtra("avaliacoes",9.2)
+             */
+
+           /* val filme = Filme(
+                "Sem limites",
+                "Teste",
+                4.8,
+                "Jamilton",
+                "Netflix"
+            )*/
+
+            //intent.putExtra("filme", filme)
+
+            startActivity( intent )
+
+        }
+
+
+
+
+
+
     }
 }
---
+++ app/src/main/java/com/luizafmartinez/m14_calculadoraimc/ResultadoActivity.kt
@@ -0,0 +1,11 @@
+package com.luizafmartinez.m14_calculadoraimc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultadoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resultado)
+    }
+}
